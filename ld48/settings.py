208d16118220df2dd8729245aa827b60af3f28ba
Andreas Säuberli <andreas.saeuberli@gmail.com>
2021-04-24 17:37:32 +0200

    production-safe settings

--- ld48/settings.py
+++ ld48/settings.py
@@ -1,17 +1,14 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8gn8zp%^--+@1st2_pa38w=2)lavfy-rwp7#!_mg(g$lo2$lpb"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "unsafe-secret-key-for-development")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "true").lower() in ["true", "yes"]
 
 ALLOWED_HOSTS = []
 
