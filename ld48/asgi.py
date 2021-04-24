a4b4905427605baf0d445d85ccac788a6a6b2830
Andreas Säuberli <andreas.saeuberli@gmail.com>
2021-04-24 09:32:29 +0200

    initial commit

---
+++ ld48/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ld48 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ld48.settings')
+
+application = get_asgi_application()
