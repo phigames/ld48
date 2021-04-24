d99fb3644a7c8019ba8ec434a0411286ae327889
Andreas Säuberli <andreas.saeuberli@gmail.com>
2021-04-24 17:25:43 +0200

    simplify ratings

--- ld48/models.py
+++ ld48/models.py
@@ -6,17 +6,13 @@ from django.db import models
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     text = models.TextField()
-    average_rating = models.FloatField(null=True)
+    average_rating = models.FloatField(default=0)
     n_ratings = models.PositiveIntegerField(default=0)
     username = models.CharField(max_length=30)
 
     def add_rating(self, rating: float):
-        if self.average_rating is None:
-            self.average_rating = rating
-            self.n_ratings = 1
-        else:
-            ratings_sum = self.average_rating * self.n_ratings
-            ratings_sum += rating
-            self.n_ratings += 1
-            self.average_rating = ratings_sum / self.n_ratings
+        ratings_sum = self.average_rating * self.n_ratings
+        ratings_sum += rating
+        self.n_ratings += 1
+        self.average_rating = ratings_sum / self.n_ratings
         self.save()
