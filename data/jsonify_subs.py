208853859d3f0bd0690ccb22081b478dc2373d79
Andreas Säuberli <andreas.saeuberli@gmail.com>
2021-04-25 18:29:21 +0200

    update data

--- data/jsonify_subs.py
+++ data/jsonify_subs.py
@@ -2,9 +2,10 @@ import json
 
 punct = [".", "?", "!", ","]
 
-with open("scores.txt") as s:#, open("alternatives.jsonl", "w") as a:
+with open("../scores.txt") as s:  # , open("alternatives.jsonl", "w") as a:
 
     quote = []
+    counter = 0
 
     for line in s:
 
@@ -14,8 +15,8 @@ with open("scores.txt") as s:#, open("alternatives.jsonl", "w") as a:
         suggestions = [sugg_score.split()[0] for sugg_score in line[1:]]
 
         for s in suggestions:
-        	if s in punct:
-        		suggestions.remove(s)
+            if s in punct:
+                suggestions.remove(s)
 
         if not any(character.isalnum() for character in original):
             suggestions = punct[:]
@@ -24,14 +25,16 @@ with open("scores.txt") as s:#, open("alternatives.jsonl", "w") as a:
             suggestions.remove(original)
 
         if "</s>" in suggestions:
-        	suggestions.remove("</s>")
+            suggestions.remove("</s>")
 
         if original.strip() == "</s>":
-            print(json.dumps(quote))
+            if counter <= 140:
+                print(json.dumps(quote))
             quote = []
+            counter = 0
             continue
 
-
+        counter += len(original) + 1
 
         # dic[original] = suggestions
         dic["word"] = original
