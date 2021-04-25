080dfd69af270f1b1f2013fe7b1bf56729d99381
Andreas Säuberli <andreas.saeuberli@gmail.com>
2021-04-25 14:54:02 +0200

    mark user ratings

--- ld48/static/ld48/js/ratings.js
+++ ld48/static/ld48/js/ratings.js
@@ -2,21 +2,25 @@ const starRatings = $(".star-rating");
 
 for (const starRating of starRatings) {
   const id = starRating.dataset.ratingId;
+  // TODO: check if already rated (in local storage), add user-rated class
   $(`.rating-star[data-rating-id='${id}']`).on("click", function (event) {
+    if (starRating.classList.contains('user-rated')) {
+      // TODO: allow changing rating
+      return;
+    }
     const value = event.target.dataset.ratingStarValue;
-    axios
-      .post(`/ratings/?id=${id}&rating=${value}`)
-      .then(function () {
-        for (let i = 1; i < 6; i++) {
-          const star = $(
-            `.rating-star[data-rating-id='${id}'][data-rating-star-value='${i}']`
-          );
-          if (i <= value) {
-            star.removeClass("bi-star").addClass("bi-star-fill");
-          } else {
-            star.removeClass("bi-star-fill").addClass("bi-star");
-          }
+    axios.post(`/ratings/?id=${id}&rating=${value}`).then(function () {
+      starRating.classList.add("user-rated");
+      for (let i = 1; i <= 5; i++) {
+        const star = $(
+          `.rating-star[data-rating-id='${id}'][data-rating-star-value='${i}']`
+        );
+        if (i <= value) {
+          star.removeClass("bi-star").addClass("bi-star-fill");
+        } else {
+          star.removeClass("bi-star-fill").addClass("bi-star");
         }
-      });
+      }
+    });
   });
 }
