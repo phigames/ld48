e42d1cb32adedaf4bab3b603f627df3cc7b3d2e7
Andreas Säuberli <andreas.saeuberli@gmail.com>
2021-04-26 16:14:41 +0200

    display formatted timedeltas

--- ld48/templatetags/utils.py
+++ ld48/templatetags/utils.py
@@ -1,4 +1,6 @@
+import babel.dates
 from django import template
+from django.utils import timezone
 
 register = template.Library()
 
@@ -6,6 +8,11 @@ register = template.Library()
 @register.filter
 def times(number: int):
     return range(1, number + 1)
+
+
+@register.filter
+def format_timedelta(time: timezone.datetime):
+    return babel.dates.format_timedelta(time - timezone.now(), add_direction=True)
 
 
 @register.inclusion_tag("ld48/star_rating.html")
